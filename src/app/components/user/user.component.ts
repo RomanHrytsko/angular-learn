e7ec0a820be9f7598db917bdb3f5f5346746d278
RomanHrytsko <romangritsko@gmail.com>
2021-01-21 12:32:46 +0200

    lesson 2 finished

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input, OnInit, Output} from '@angular/core';
+import {User} from '../../../models/User';
+import {EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  @Input()
+  user: User;
+
+  @Output()
+  bubbleUp = new EventEmitter<User>();
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  getUserInfo(user: User): void {
+    this.bubbleUp.emit(user);
+  }
+}
