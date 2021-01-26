cbf7de1180cacac14e58c93ad1996788bc211327
RomanHrytsko <romangritsko@gmail.com>
2021-01-26 16:43:10 +0200

    lesson finished

---
+++ src/app/models/Address.ts
@@ -0,0 +1,16 @@
+// address: {
+//   street: "Kulas Light",
+//     suite: "Apt. 556",
+//     city: "Gwenborough",
+//     zipcode: "92998-3874",
+//     geo: {
+//     lat: "-37.3159",
+//       lng: "81.1496",
+//   },
+// }
+
+export interface Address {
+  street: string;
+  city: string;
+  geo: { lat: string, lng: string };
+}
