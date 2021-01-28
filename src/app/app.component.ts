a1ce9355b16ec4bcb32cd85c4fbe4fba70465a6e
RomanHrytsko <romangritsko@gmail.com>
2021-01-28 07:38:00 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularLearn';
+
+  user = {name: 'vasia', age: 124};
+
+//   onInputAction(input: HTMLInputElement): void {
+//     console.log(this.title);
+//     console.log(input);
+//   }
+// }
+//
+
+//   submit(form: HTMLFormElement): void {
+//     console.log(form);
+//   }
+
+//   submit(form: NgForm): void {
+//     console.log(form);
+//   }
+
+
+  // name = new FormControl('vasia');
+  // age = new FormControl(123);
+  // myForm = new FormGroup({
+  //   name: this.name,
+  //   age: this.age
+  // });
+  //
+  // xxx(myForm: FormGroup): void {
+  //   console.log(myForm);
+  // }
+
+  name = new FormControl('', [Validators.required, Validators.minLength(10), this.customValidator]);
+  age = new FormControl('');
+  myForm = new FormGroup({
+    name: this.name,
+    age: this.age
+  });
+
+
+  xxx(myForm: FormGroup): void {
+    console.log(myForm);
+  }
+
+  customValidator(inputData: AbstractControl): any {
+    console.log('test');
+    if (inputData.value === 'fuck') {
+      console.log(inputData);
+
+      return {error: true, msg: 'fuck is present'};
+    } else {
+      return null;
+    }
+  }
+
+
 }
+
