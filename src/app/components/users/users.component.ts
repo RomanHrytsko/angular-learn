344685620c28f6ddf2dce3d6c64f16a0eda5229f
RomanHrytsko <romangritsko@gmail.com>
2021-01-29 19:00:45 +0200

    improved

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
-import {Users} from '../../Data/Users';
+import {Users} from '../../Models/Users';
 
 
 @Component({
@@ -13,33 +13,10 @@ export class UsersComponent implements OnInit {
   usersArr = [];
   userArr: Users;
 
-
-  form: FormGroup;
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-
-    // this.form = new FormGroup({
-    //   name: new FormControl(''),
-    //   age: new FormControl('', [this.ageValidator])
-    // });
-  }
-
-
-  // submit(form: NgForm): void {
-  //
-  //   console.log(form);
-  //   this.usersArr.push({name: form.controls.name.value, age: form.controls.age.value, email: form.controls.email.value});
-  //   console.log(this.usersArr);
-  //
-  // }
-
-
   name = new FormControl('', Validators.minLength(4));
   age = new FormControl(0, [Validators.required, this.ageValidator]);
   email = new FormControl('', Validators.email);
+  form: FormGroup;
 
   inputs = new FormGroup({
     name: this.name,
@@ -47,11 +24,25 @@ export class UsersComponent implements OnInit {
     email: this.email
   });
 
-  addNewUser(inputs): void {
-    this.usersArr.push({name: this.name.value, age: this.age.value, email: this.email.value});
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  addNewUser(inputs: FormGroup): void {
+    this.usersArr.push(inputs.getRawValue());
     console.log(inputs);
+    console.log(inputs.getRawValue());
 
   }
+
+  // addNewUser(inputs): void {
+  //   this.usersArr.push({name: this.name.value, age: this.age.value, email: this.email.value});
+  //   console.log(inputs);
+  //
+  // }
 
   ageValidator(inputData: AbstractControl): { ageError: boolean } | null {
     if (inputData.value <= 15) {
